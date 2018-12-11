07383bb2f182bb254e36578ba8bcf3b4e6516967
AnupSukumaran <32585771+AnupSukumaran@users.noreply.github.com>
2018-12-11 16:21:02 +0530

    Initial Commit

---
+++ TimeTester/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeTester
+//
+//  Created by Abraham VG on 11/12/18.
+//  Copyright © 2018 Wis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
